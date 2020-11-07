250d9920ff121507cd8a3d575424f6c1cad23183
BlazejMaciejski <56317717+BlazejMaciejski@users.noreply.github.com>
2020-11-07 16:07:42 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -8,7 +8,10 @@ private:
     Resource& managedResource;
 
 public:
-    ResourceManager() : managedResource(*new Resource) {}
+    ResourceManager() : managedResource(*new Resource)
+    {
+        std::cout << this << "\n Podstawowy konstruktor \n";
+    }
 
     /*
     ResourceManager(Resource& resource) : managedResource(resource)
@@ -19,19 +22,26 @@ public:
     */
 
     // konstruktor kopiujący
-    ResourceManager(const ResourceManager& manager) : managedResource(manager.managedResource){};
+    ResourceManager(const ResourceManager& manager)
+        : managedResource(*new Resource = manager.managedResource)
+    {
+        std::cout << this << "\n Kopiujący konstruktor \n";
+    };
 
     // konstruktor przenoszący
     ResourceManager(ResourceManager&& manager) : managedResource(manager.managedResource) {}
 
     ~ResourceManager()
     {
+        std::cout << this << "\n  Destruktor  \n";
+        delete &managedResource;
         //.freeResources();
     }
 
     // Kopiujący operator przypisania
     ResourceManager& operator=(const ResourceManager& t)
     {
+        std::cout << this << "\n Kopiujący operantor przenoszenia \n";
         managedResource = t.managedResource;
         return (*this);
     };
