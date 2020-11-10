6be87e0b0b07aa51c9199583cdf1bece0708cacd
BlazejMaciejski <56317717+BlazejMaciejski@users.noreply.github.com>
2020-11-10 08:19:40 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -5,10 +5,10 @@
 class ResourceManager
 {
 private:
-    Resource& managedResource;
+    Resource* managedResource;
 
 public:
-    ResourceManager() : managedResource(*new Resource) {}
+    ResourceManager() : managedResource(new Resource) {}
 
     /*
     ResourceManager(Resource& resource) : managedResource(resource)
@@ -20,25 +20,99 @@ public:
 
     // konstruktor kopiujący
     ResourceManager(const ResourceManager& manager)
-        : managedResource(*new Resource = manager.managedResource){}
+        : managedResource(new Resource(*manager.managedResource))
+    {}
 
     // konstruktor przenoszący
-    ResourceManager(ResourceManager&& manager) : managedResource(manager.managedResource) {}
+    ResourceManager(ResourceManager&& manager) : managedResource(manager.managedResource)
+    {
+        manager.managedResource = nullptr;
+    }
 
     ~ResourceManager()
     {
-        delete & managedResource;
+        delete managedResource;
         //.freeResources();
     }
 
     // Kopiujący operator przypisania
     ResourceManager& operator=(const ResourceManager& t)
     {
-        managedResource = t.managedResource;
-        return (*this);
-    };
-    // Przenoszący operator przypisania
-    ResourceManager& operator=(ResourceManager&&);
+        // if (this != t)
+        {
+            delete (this->managedResource);
+            (*this).managedResource = new Resource(*t.managedResource);
+            return (*this);
+        }
+    }
+;
 
-    double get() { return (managedResource.get()); }
-};
+    // Przenoszący operator przypisania
+    ResourceManager& operator=(ResourceManager&& t)
+    {
+        // if (this != t)
+        {
+            delete (this->managedResource);
+            (*this).managedResource = t.managedResource;
+            t.managedResource        = nullptr;
+            return (*this);
+        }
+    };
+
+    double get()
+    {
+        return ((*managedResource).get());
+    }
+}
+;
+
+
+
+
+
+
+
+//#pragma once
+//
+//#include "Resource.hpp"
+//
+//class ResourceManager
+//{
+//private:
+//    Resource& managedResource;
+//
+//public:
+//    ResourceManager() : managedResource(*new Resource) {}
+//
+//    /*
+//    ResourceManager(Resource& resource) : managedResource(resource)
+//    {
+//
+//        //.allocateResources();
+//    }
+//    */
+//
+//    // konstruktor kopiujący
+//    ResourceManager(const ResourceManager& manager)
+//        : managedResource(*new Resource = manager.managedResource){}
+//
+//    // konstruktor przenoszący
+//    ResourceManager(ResourceManager&& manager) : managedResource(manager.managedResource) {}
+//
+//    ~ResourceManager()
+//    {
+//        delete & managedResource;
+//        //.freeResources();
+//    }
+//
+//    // Kopiujący operator przypisania
+//    ResourceManager& operator=(const ResourceManager& t)
+//    {
+//        managedResource = t.managedResource;
+//        return (*this);
+//    };
+//    // Przenoszący operator przypisania
+//    ResourceManager& operator=(ResourceManager&&);
+//
+//    double get() { return (managedResource.get()); }
+//};
